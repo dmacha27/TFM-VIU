b727fcbda7194a4e20db481d3c0839d94fcf4d0b
Acha <achacbmb3@gmail.com>
2024-08-20 13:40:44 +0200

    Actualizar secretkey

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -4,7 +4,7 @@ from flask_compress import Compress
 def create_app():
     app = Flask(__name__)
     Compress(app)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SECRET_KEY'] = 'ac6c836efab24ca0a75d485e8b9a9f6e'
 
     from .views import views
 
